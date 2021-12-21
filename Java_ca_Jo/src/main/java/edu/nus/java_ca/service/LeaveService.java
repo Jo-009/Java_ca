c1ae1b4a1d5ae44748fffaa8449576b795161282
Jo-009 <94837678+Jo-009@users.noreply.github.com>
2021-12-21 19:21:21 +0800

    Add files via upload

---
+++ Java_ca_Jo/src/main/java/edu/nus/java_ca/service/LeaveService.java
@@ -0,0 +1,33 @@
+package edu.nus.java_ca.service;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+import edu.nus.java_ca.model.Department;
+import edu.nus.java_ca.model.Leave;
+
+public interface LeaveService {
+	
+ 
+	//for applying
+	Leave createLeave(Leave l);
+	Leave changeLeave(Leave l);
+	public void cancelLeave(Leave l);
+	
+	//for managers only
+	public List<Leave> listLeavesByUserId(Long id);
+	ArrayList<Leave> findAppliedLeaves();
+	public List<Leave> listAllLeaves();
+	public List<Leave> listLeaveToApprove();
+	public List<Leave> listLeaveToApprove(Department d);
+	public void approveLeave(Leave l);
+	public void rejectLeave(Leave l);
+	public Leave findLeaveById(Long id);
+	public List<Leave> findLeavesByDate(LocalDate d);
+	public List<Leave> findLeavesByYearandMonth(int yy, int mm);	
+	//for staff
+	Leave findByStartDateAndEndDate(LocalDate s, LocalDate e);
+	Long countLeaves(LocalDate s, LocalDate e);
+	Boolean checkDupes(LocalDate s, LocalDate e);
+}
---
+++ Java_ca_Jo/src/main/java/edu/nus/java_ca/validator/LeaveValidator.java
@@ -0,0 +1,48 @@
+package edu.nus.java_ca.validator;
+
+import java.util.Calendar;
+
+import org.springframework.validation.Errors;
+import org.springframework.validation.ValidationUtils;
+import org.springframework.validation.Validator;
+
+import edu.nus.java_ca.model.Leave;
+
+public class LeaveValidator implements Validator {
+
+	@Override
+	public boolean supports(Class<?> clazz) {
+		// TODO Auto-generated method stub
+		return Leave.class.equals(clazz);
+	}
+
+	@Override
+	public void validate(Object target, Errors e) {
+		// TODO Auto-generated method stub
+		Leave l = (Leave) target;
+
+		if ((l.getStartDate()!=null && l.getEndDate()!=null)&&(l.getStartDate().compareTo(l.getEndDate())>=0)) {
+		
+			e.rejectValue("startDate", "error.startDate", "**StartDate must be Before EndDate**");
+			e.rejectValue("endDate", "error.endDate", "**EndDate must be After StartDate**");
+		}
+	
+		ValidationUtils.rejectIfEmptyOrWhitespace(e, "reason", "error.reason","**Reason is Required.**");
+		ValidationUtils.rejectIfEmptyOrWhitespace(e, "startDate", "error.startDate", "**Start Date is Required.**");
+		ValidationUtils.rejectIfEmptyOrWhitespace(e, "endDate", "error.endDate", "**End Date is Required.**");
+		ValidationUtils.rejectIfEmptyOrWhitespace(e, "type", "error.type","**Leave Type is Required.**");
+		
+		if(l.getStartDate()!=null) {
+			int i = l.getStartDate().getDayOfWeek().getValue();
+			if ((i==6||i==7)) {
+			e.rejectValue("startDate", "error.startDate", "**StartDate must be Weekdays**");}
+		}
+		
+		if(l.getEndDate()!=null) {
+			int j = l.getEndDate().getDayOfWeek().getValue();
+			if (j==6||j==7) {
+				e.rejectValue("endDate", "error.endDate", "**EndDate must be Weekdays**");}
+		}
+	}
+
+}
